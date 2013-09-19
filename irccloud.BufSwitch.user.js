ddacd074581f17dd64062ffb1e66598ea2aae939
Raine Virta <raine.virta@gmail.com>
2013-09-19 19:00:10 +0300

    bump version

--- irccloud.BufSwitch.user.js
+++ irccloud.BufSwitch.user.js
@@ -3,7 +3,7 @@
 // @namespace      http://github.com/raneksi
 // @description    Switch buffers quickly without a mouse
 // @match          https://www.irccloud.com/*
-// @version        0.0.1
+// @version        0.0.2
 // @updateURL      https://userscripts.org/scripts/source/177846.meta.js
 // @downloadURL    https://userscripts.org/scripts/source/177846.user.js
 // ==/UserScript==
